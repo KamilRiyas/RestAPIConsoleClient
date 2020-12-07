719e1f358446bf073a7f54849f3764419b7f157b
Kamil <kshameem@tmp.com>
2020-12-08 03:34:31 +0530

    Updated for multiple items

--- RestAPIConsoleClient/Program.cs
+++ RestAPIConsoleClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace RestAPIConsoleClient
 {
@@ -32,12 +33,50 @@ namespace RestAPIConsoleClient
 
             }
         }
+
+        static async Task GetAllItemsAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/todos/");
+                HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+                if (response.IsSuccessStatusCode)
+                {
+                    List<todoItem> itemsList = new List<todoItem>();
+                    itemsList = await JsonSerializer.DeserializeAsync<List<todoItem>>(await response.Content.ReadAsStreamAsync());
+                    foreach(var item in itemsList)
+                    {
+                        Console.WriteLine($"Item Id    : {item.id}");
+                        Console.WriteLine($"User Id    : {item.userId}");
+                        Console.WriteLine($"Title      : {item.title}");
+                        Console.WriteLine(item.completed ? "Status     :Completed" : "Status     :Pending");
+                    }
+                }
+
+            }
+        }
+
         static void Main(string[] args)
         {
-            int itemId = 0;
-            Console.WriteLine("Please enter the Id to be retieved");
-            itemId = Convert.ToInt32(Console.ReadLine());
-            GetItemAsync(itemId).Wait();
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1. Get one item");
+            Console.WriteLine("2. Get all items");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    int itemId = 0;
+                    Console.WriteLine("Please enter the Id to be retieved");
+                    itemId = Convert.ToInt32(Console.ReadLine());
+                    GetItemAsync(itemId).Wait();
+                    break;
+                case 2:
+                    GetAllItemsAsync().Wait();
+                    break;
+                default:
+                    break;
+            }
+
             Console.ReadLine();
         }
     }
